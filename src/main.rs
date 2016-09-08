d7e35a73e9476b5610e79ecb89f296395e7aab03
Chris MacNaughton <chmacnaughton@gmail.com>
2016-09-08 13:05:48 -0700

    switch to tarpc

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
+// extern crate futures;
 #[macro_use]
-extern crate gj;
-extern crate gjio;
-extern crate capnp;
-extern crate capnp_rpc;
-extern crate backup_capnp;
+extern crate tarpc;
 
-use backup_capnp::backup_capnp::backup;
-use gj::{Promise, EventLoop, TaskReaper, TaskSet};
+// use tarpc::util::Never;
+// use tarpc::sync::Connect;
 
-pub mod server;
+service! {
+    rpc has_full(name: String) -> bool;
+}
+
+#[derive(Clone)]
+struct BackupServer;
+
+impl Service for BackupServer {
+    fn has_full(&self, name: String) -> bool {
+        false
+    }
+}
 
 fn main() {
-    let args: Vec<String> = ::std::env::args().collect();
-    if args.len() != 3 {
-        println!("usage: {} server ADDRESS[:PORT]", args[0]);
-        return;
-    }
+    let addr = "localhost:10000";
+    let server = BackupServer.spawn(addr).unwrap();
+    let client = Client::new(addr).unwrap();
+    println!("has full: {}", client.has_full("Mom".to_string()).unwrap());
 
-    EventLoop::top_level(move |wait_scope| -> Result<(), ::capnp::Error> {
-            use std::net::ToSocketAddrs;
-            let mut event_port = try!(::gjio::EventPort::new());
-            let network = event_port.get_network();
-            let addr = try!(args[2].to_socket_addrs()).next().expect("could not parse address");
-            let mut address = network.get_tcp_address(addr);
-            let listener = try!(address.listen());
-
-            let back = backup::ToClient::new(server::Backup {})
-                .from_server::<::capnp_rpc::Server>();
-
-            let task_set = TaskSet::new(Box::new(server::Reaper));
-            try!(server::accept_loop(listener, task_set, back).wait(wait_scope, &mut event_port));
-
-            Ok(())
-        })
-        .expect("top level error");
+    drop(client);
+    server.shutdown();
 }
